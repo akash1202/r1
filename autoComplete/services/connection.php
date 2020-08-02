f0fde26dca51e8d00d86506959ecf32090d62b06
Akash Anaghan <anaghanakash007@gmail.com>
2020-08-02 17:04:50 -0400

    Update connection.php
    data added

--- autoComplete/services/connection.php
+++ autoComplete/services/connection.php
@@ -1,13 +1,13 @@
 <?php
-$host="db4free.net:3307";
-$user="akash";
-$pswd="akash123";
-$db="dbakash";
+$host="localhost";
+$user="root";
+$pswd="";
+$db="test";
 $conn=mysqli_connect($host,$user,$pswd,$db) or die("Connection failed!!");
 if($conn){
-$q1=$_POST['searchtxt'];
+$q1=$_REQUEST['searchtxt'];
 //	echo "conection esatablished!!!!!!!";
-$result=mysqli_query($conn,"SELECT * from apps_countries where country_name like '$q1%'");
+$result=mysqli_query($conn,"SELECT * from country where name like '$q1%'");
 $arr=array();
 if(mysqli_num_rows($result)>0){
 header("Content-type:application:json;charset:UTF-8");	
